108586d3542f9236e00564263adb87c68d0089ad
mijburgh-beukes <mijburgh.beukes@gmail.com>
2024-03-25 23:16:09 +1300

    feat: use hono to get the server working

--- index.tsx
+++ index.tsx
@@ -1,26 +1,58 @@
-/* import { Elysia } from "elysia";
-
-const app = new Elysia().get("/", () => "Hello World!").listen(3000); */
-// console.log(`Elysia is running at http://`);
+import { Hono } from "hono";
+import { html } from "hono/html";
 import * as elements from "typed-html";
-const server = Bun.serve({
-  port: 3000,
-  fetch(request) {
-    return new Response("Welcome to Bun!");
-  },
-});
 
-console.log(`Listening on localhost:${server.port}`);
+const app = new Hono();
+app
+  .get("/", (c) =>
+    c.html(
+      <BaseHtml>
+        <button hx-post="/clicked" hx-swap="outerHTML">
+          Click me
+        </button>
+      </BaseHtml>
+    )
+  )
+  .post("/clicked", (c) =>
+    c.html(<div class="text-blue-600">I'm from the server!</div>)
+  );
 
-const base = ({ children }: elements.Children) => `<!DOCTYPE html>
+export default app;
+
+// const server = Bun.serve({
+//   hostname: "localhost",
+//   port: 3000,
+//   fetch: handler,
+//   error(error) {
+//     console.log(error);
+//     return new Response("oops");
+//   },
+// });
+
+// function handler(req: Request): Response {
+//   const url = new URL(req.url);
+//   switch (url.pathname) {
+//     case "":
+//     case "/":
+//       return new Response(Bun.file("index.html"));
+//     default:
+//       break;
+//   }
+//   return new Response("Not found", { status: 404 });
+// }
+
+// console.log(`Listening on localhost:${server.hostname}:${server.port}`);
+
+const BaseHtml = ({ children }: elements.Children) => `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="css/style.css" rel="stylesheet">
+  <script src="https://unpkg.com/htmx.org@1.9.11" integrity="sha384-0gxUXCCR8yv9FM2b+U3FDbsKthCI66oH5IA9fHppQq9DDMHuMauqq1ZHBpJxQ0J0" crossorigin="anonymous"></script>
+  <script src="https://cdn.tailwindcss.com"></script>
   <title>EV Charge Time</title>
 </head>
-<body>
+<body class="flex w-full h-screen justify-center items-center">
   ${children}
 </body>
 </html>`;
