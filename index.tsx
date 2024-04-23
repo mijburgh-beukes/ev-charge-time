b71f03f9acfd3608f6beb96908a108108401f21c
mijburgh-beukes <mijburgh.beukes@gmail.com>
2024-04-24 01:13:46 +1200

    feat: add more routes

--- index.tsx
+++ index.tsx
@@ -9,11 +9,45 @@ app
         {/* <button hx-post="/clicked" hx-swap="outerHTML">
           Click me
         </button> */}
-        <Select {...selectManufacturer} />
+        <div class="flex space-x-4">
+          <Select {...selectManufacturer} />
+          {/* <button
+            hx-post="model"
+            hx-swap="outerHTML"
+            hx-target="closest div"
+            class="bg-gray-200 rounded-md px-4"
+          >
+            Push me
+          </button> */}
+        </div>
+        {/* <Select {...selectModel} /> */}
       </BaseHtml>
     )
   )
-  .post("/clicked", (c) => c.html(Select(selectManufacturer)));
+  .post("/manufacturer", async (c) => {
+    const mf = (await c.req.formData()).get("manufacturer");
+
+    const props: SelectProps = {
+      name: "model",
+      id: "md-select",
+      options: [],
+    };
+
+    switch (mf) {
+      case "nissan":
+        props.options = ["leaf", "ariya"];
+        break;
+
+      default:
+        break;
+    }
+    console.log(props);
+    return c.html(
+      <div class="flex space-x-2">
+        <Select {...props} />
+      </div>
+    );
+  });
 
 export default app;
 
@@ -30,6 +64,12 @@ const selectManufacturer: SelectProps = {
   options: ["nissan", "tesla", "bmw", "audi"],
 };
 
+const selectModel: SelectProps = {
+  name: "model",
+  id: "md-select",
+  options: ["leaf", "ariya"],
+};
+
 function Select(props: SelectProps) {
   const { name, id, options, className = "" } = props;
   return (
@@ -37,6 +77,10 @@ function Select(props: SelectProps) {
       name={name}
       id={id}
       class={`${className} border-b-2 border-x-0 border-t-0 border-slate-200 capitalize  focus:ring-0 focus:ring-transparent focus:border-slate-200 hover:shadow-lg transition-shadow`}
+      hx-trigger="change changed"
+      hx-post={`/${name}`}
+      hx-target="closest div"
+      hx-swap="outerHTML transition:true"
     >
       <option value="">--{name}--</option>
       {options.map((option) => (
@@ -46,37 +90,17 @@ function Select(props: SelectProps) {
   );
 }
 
-// const server = Bun.serve({
-//   hostname: "localhost",
-//   port: 3000,
-//   fetch: handler,
-//   error(error) {
-//     console.log(error);
-//     return new Response("oops");
-//   },
-// });
-
-// function handler(req: Request): Response {
-//   const url = new URL(req.url);
-//   switch (url.pathname) {
-//     case "":
-//     case "/":
-//       return new Response(Bun.file("index.html"));
-//     default:
-//       break;
-//   }
-//   return new Response("Not found", { status: 404 });
-// }
-
-// console.log(`Listening on localhost:${server.hostname}:${server.port}`);
-
 const BaseHtml = ({ children }: elements.Children) => `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@300;600&display=swap" rel="stylesheet">
   <script src="https://unpkg.com/htmx.org@1.9.11" integrity="sha384-0gxUXCCR8yv9FM2b+U3FDbsKthCI66oH5IA9fHppQq9DDMHuMauqq1ZHBpJxQ0J0" crossorigin="anonymous"></script>
   <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+  <link href="css/style.css" rel="stylesheet" >
   <title>EV Charge Time</title>
 </head>
 <body class="flex w-full h-screen justify-center items-center">
